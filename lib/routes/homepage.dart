7a03263834b6ac421adc777be076d7b8a4e181b2
busrahadimlioglu <busrahadimlioglu@gmail.com>
2023-12-16 20:32:08 +0300

    pages added

---
+++ lib/routes/homepage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_case_study/routes/settingspage.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+    );
+  }
+}
