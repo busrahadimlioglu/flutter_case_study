7f9f4e1ac9731e448b3e65b62ee43c280d677d49
busrahadimlioglu <busrahadimlioglu@gmail.com>
2023-12-19 20:57:39 +0300

    settings page updated

--- lib/screens/settingsPage.dart
+++ lib/screens/settingsPage.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import '../models/settings.dart';
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({Key? key}) : super(key: key);
 
   @override
   State<SettingsPage> createState() => _SettingsPageState();
@@ -16,48 +16,45 @@ class _SettingsPageState extends State<SettingsPage> {
     String osVersion = getOSVersion();
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  children: List.generate(
-                    settings.length,
-                        (index) => SettingTile(setting: settings[index]),
+        padding: const EdgeInsets.fromLTRB(8,70,8,0),
+        child: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            if (index < settings.length) {
+              return SettingTile(setting: settings[index]);
+            } else {
+              return Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: const Icon(Icons.cable_outlined, color: Colors.black),
                   ),
-                ),
-                Row(
-                  children: [
-                    Container(
-                      height: 50,
-                      width: 50,
-                      margin: const EdgeInsets.only(bottom: 10),
-                      child: const Icon(Icons.cable_outlined, color: Colors.black),
+                  const SizedBox(width: 15),
+                  const Text(
+                    "OS Version",
+                    style: TextStyle(
+                      color: Color(0xff000000),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
                     ),
-                    const SizedBox(width: 15),
-                    const Text(
-                      "OS Version",
-                      style: TextStyle(
-                        color: Color(0xff000000),
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
+                  ),
+                  const Spacer(),
+                  Text(
+                    osVersion,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Color(0xff838282),
                     ),
-                    const Spacer(),
-                    Text(
-                      osVersion,
-                      style: const TextStyle(fontSize: 14,
-                        color: Color(0xff838282)
-                      ),
-                    ),
-
-                  ],
-                ),
-              ],
-            ),
-          ),
+                  ),
+                ],
+              );
+            }
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return const Divider(); // Add a Divider between tiles
+          },
+          itemCount: settings.length + 1, // Add 1 for the additional widgets after the tiles
         ),
       ),
     );
